c9db8832fdbe1b6b725a7bffd22a17c2c9149131
Wolfgang Mathurin <wmathurin@salesforce.com>
2012-10-15 15:35:38 -0700

    Force plugins order not assumed - instead we sort them

--- PhoneGap/test/SFSDKInfoTestSuite.js
+++ PhoneGap/test/SFSDKInfoTestSuite.js
@@ -54,20 +54,21 @@ SDKInfoTestSuite.prototype.testGetInfo = function()  {
     console.log("In SFSDKInfoTestSuite.testGetInfo");
 
     var self = this;
-
+    
     self.getInfo()
-        .done(function(sdkInfo) {
-            QUnit.equal(sdkInfo.sdkVersion.indexOf("2.0"), 0, "expected different sdk version");
-            QUnit.equal(sdkInfo.appName, "ForcePluginsTest", "expected different app name");
-            QUnit.equal(sdkInfo.appVersion, "1.0", "expected different app version");
-            QUnit.equal(sdkInfo.forcePluginsAvailable.length, 4, "wrong force plugins");
-            QUnit.equal(sdkInfo.forcePluginsAvailable[0], "com.salesforce.oauth", "wrong force plugins");
-            QUnit.equal(sdkInfo.forcePluginsAvailable[1], "com.salesforce.sdkinfo", "wrong force plugins");
-            QUnit.equal(sdkInfo.forcePluginsAvailable[2], "com.salesforce.testrunner", "wrong force plugins");
-            QUnit.equal(sdkInfo.forcePluginsAvailable[3], "com.salesforce.smartstore", "wrong force plugins");
-            self.finalizeTest();
-        });
-}; 
+    .done(function(sdkInfo) {
+          QUnit.equal(sdkInfo.sdkVersion.indexOf("1.4"), 0, "expected different sdk version");
+          QUnit.equal(sdkInfo.appName, "ForcePluginsTest", "expected different app name");
+          QUnit.equal(sdkInfo.appVersion, "1.0", "expected different app version");
+          QUnit.equal(sdkInfo.forcePluginsAvailable.length, 4, "wrong force plugins");
+          sdkInfo.forcePluginsAvailable.sort();
+          QUnit.equal(sdkInfo.forcePluginsAvailable[0], "com.salesforce.oauth", "wrong force plugins");
+          QUnit.equal(sdkInfo.forcePluginsAvailable[1], "com.salesforce.sdkinfo", "wrong force plugins");
+          QUnit.equal(sdkInfo.forcePluginsAvailable[2], "com.salesforce.smartstore", "wrong force plugins");
+          QUnit.equal(sdkInfo.forcePluginsAvailable[3], "com.salesforce.testrunner", "wrong force plugins");
+          self.finalizeTest();
+          });
+};
 
 }
 
